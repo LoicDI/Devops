cde7857fbdb235afa7a1489377d646d5af5dc555
Loic dietmann <loic.dietmann@viacesi.fr>
2023-07-06 10:02:03 +0200

    dockrimage3

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@ $servername = "localhost"; // Server name where the app has the DB
 $username = "root"; // user of the DB 
 $password = "root"; // password of the DB
 $dbname = "mono"; // DB name 
-
+ 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection

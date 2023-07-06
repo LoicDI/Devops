b9292afc75dd587152b62065809e5ce1e0c90478
Loic dietmann <loic.dietmann@viacesi.fr>
2023-07-06 11:13:14 +0200

    deploy2

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; // Server name where the app has the DB
 $username = "root"; // user of the DB 
 $password = "root"; // password of the DB
-$dbname = "mono"; // DB name 
+$dbname = "mono"; // DB name
  
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
